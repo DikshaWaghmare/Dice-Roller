4d52107497f2e794c043b51760ba072b6e788be7
DikshaWaghmare <dikshaw44@gmail.com>
2022-01-30 14:02:17 +0530

    add file

---
+++ diceRoller.js
@@ -0,0 +1,22 @@
+function randomDice() {
+    var randomNo= Math.floor(Math.random() * 6) + 1;
+    document.getElementById("ans").innerHTML=randomNo;
+    if(randomNo==1){
+     document.getElementById("dice").innerHTML='<img class="img" src ="./JavaScript/1-dice.jpg">';
+    }
+    if(randomNo==2){
+      document.getElementById("dice").innerHTML='<img class="img" src ="./JavaScript/2-dice.jpg">';
+    }
+    if(randomNo==3){
+      document.getElementById("dice").innerHTML='<img class="img" src ="./JavaScript/3-dice.jpg">';
+    }
+    if(randomNo==4){
+      document.getElementById("dice").innerHTML='<img class="img" src ="./JavaScript/4-dice.jpg">';
+    }
+    if(randomNo==5){
+      document.getElementById("dice").innerHTML='<img class="img" src ="./JavaScript/5-dice.jpg">';
+    }
+    if(randomNo==6){
+      document.getElementById("dice").innerHTML='<img class="img" src ="./JavaScript/6-dice.jpg">';
+    }
+  }
