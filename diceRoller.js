04e68c42c37dd8f27923d1db3e17af11d1926f2d
DikshaWaghmare <dikshaw44@gmail.com>
2022-01-30 14:35:47 +0530

    change img path

--- diceRoller.js
+++ diceRoller.js
@@ -2,21 +2,21 @@ function randomDice() {
     var randomNo= Math.floor(Math.random() * 6) + 1;
     document.getElementById("ans").innerHTML=randomNo;
     if(randomNo==1){
-     document.getElementById("dice").innerHTML='<img class="img" src ="./JavaScript/1-dice.jpg">';
+     document.getElementById("dice").innerHTML='<img class="img" src ="1-dice.jpg">';
     }
     if(randomNo==2){
-      document.getElementById("dice").innerHTML='<img class="img" src ="./JavaScript/2-dice.jpg">';
+      document.getElementById("dice").innerHTML='<img class="img" src ="2-dice.jpg">';
     }
     if(randomNo==3){
-      document.getElementById("dice").innerHTML='<img class="img" src ="./JavaScript/3-dice.jpg">';
+      document.getElementById("dice").innerHTML='<img class="img" src ="3-dice.jpg">';
     }
     if(randomNo==4){
-      document.getElementById("dice").innerHTML='<img class="img" src ="./JavaScript/4-dice.jpg">';
+      document.getElementById("dice").innerHTML='<img class="img" src ="4-dice.jpg">';
     }
     if(randomNo==5){
-      document.getElementById("dice").innerHTML='<img class="img" src ="./JavaScript/5-dice.jpg">';
+      document.getElementById("dice").innerHTML='<img class="img" src ="5-dice.jpg">';
     }
     if(randomNo==6){
-      document.getElementById("dice").innerHTML='<img class="img" src ="./JavaScript/6-dice.jpg">';
+      document.getElementById("dice").innerHTML='<img class="img" src ="6-dice.jpg">';
     }
   }
