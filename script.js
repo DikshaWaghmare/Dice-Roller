1d3f51b0bca7c4f62e324777017169dff957cedc
DikshaWaghmare <dikshaw44@gmail.com>
2022-02-03 15:34:21 +0530

    update files

--- script.js
+++ script.js
@@ -2,21 +2,21 @@ function randomDice() {
     var randomNo= Math.floor(Math.random() * 6) + 1;
     document.getElementById("ans").innerHTML=randomNo;
     if(randomNo==1){
-     document.getElementById("dice").innerHTML='<img class="img" src ="./images/1-dice.jpg">';
+     document.getElementById("dice").innerHTML='<img class="img" src ="./assets/images/1-dice.jpg">';
     }
     if(randomNo==2){
-      document.getElementById("dice").innerHTML='<img class="img" src ="./images/2-dice.jpg">';
+      document.getElementById("dice").innerHTML='<img class="img" src ="./assets/images/2-dice.jpg">';
     }
     if(randomNo==3){
-      document.getElementById("dice").innerHTML='<img class="img" src ="./images/3-dice.jpg">';
+      document.getElementById("dice").innerHTML='<img class="img" src ="./assets/images/3-dice.jpg">';
     }
     if(randomNo==4){
-      document.getElementById("dice").innerHTML='<img class="img" src ="./images/4-dice.jpg">';
+      document.getElementById("dice").innerHTML='<img class="img" src ="./assets/images/4-dice.jpg">';
     }
     if(randomNo==5){
-      document.getElementById("dice").innerHTML='<img class="img" src ="./images/5-dice.jpg">';
+      document.getElementById("dice").innerHTML='<img class="img" src ="./assets/images/5-dice.jpg">';
     }
     if(randomNo==6){
-      document.getElementById("dice").innerHTML='<img class="img" src ="./images/6-dice.jpg">';
+      document.getElementById("dice").innerHTML='<img class="img" src ="./assets/images/6-dice.jpg">';
     }
   }
